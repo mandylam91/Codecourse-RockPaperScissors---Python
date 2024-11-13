fab56f208b1f2ae1676bdc3c5a51222963570c27
mandylam91 <54041104+mandylam91@users.noreply.github.com>
2024-11-13 20:29:12 +0000

    Adding additional comments

--- CodeCourse Intro Python RockPaperScissors.py
+++ CodeCourse Intro Python RockPaperScissors.py
@@ -6,6 +6,7 @@ def RockPaperScissors():
     compscore = 0
     yourscore = 0
     playagain = 'y'
+    #Code to get game to continue to loop round and re run until user doenst want to play anymore
     while playagain == 'y':
         # user to input choice
         userchoice = input("Enter a choice (Rock(r), Paper(p), Scissors(s))")
@@ -16,7 +17,7 @@ def RockPaperScissors():
         else:
             choice = 'scissors'
 
-        # get computer to chose a random number between 1 and 3
+        # get computer to chose a random number between 1 and 3 and set each number to equal a different choice
         n = (random.randint(1, 3))
         if n == 1:
             compchoice = 'rock'
@@ -26,7 +27,7 @@ def RockPaperScissors():
             compchoice = 'scissors'
         #print your choice and computers choice
         print('Your Choice is ' + choice + ', Computers choice is ' + compchoice)
-
+        #The result of different choices and adding a point to the winner
         if choice == compchoice:
             result = 'Its a tie!'
         elif choice == 'rock' and compchoice == 'paper':
@@ -51,7 +52,7 @@ def RockPaperScissors():
         print(result)
         print('Computers Score is: ' + str(compscore))
         print('Your Score is: ' + str(yourscore))
-
+        #Ask user whether they want t play again, while loop at the top of code to reloop if user selects y
         playagain = input("Do you want to play again? y/n?")
 
     else:
